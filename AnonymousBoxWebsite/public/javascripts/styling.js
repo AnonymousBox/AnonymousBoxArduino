edcd7020a73a806aabb0db3f54e3a1103872bbab
Jake Schievink <jakeschievink@gmail.com>
2014-02-18 19:33:41 -0500

    first little fixes

---
+++ AnonymousBoxWebsite/public/javascripts/styling.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(document.body).css('padding-top', $('#topnavbar').height() + 40);
+    $('.rimg').resizecrop({
+              width:200,
+              height:200
+    });  
+});
