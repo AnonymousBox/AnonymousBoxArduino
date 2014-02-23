41f43ff9791de9fd713ad5477bb54fa89e40f688
Jake Schievink <jakeschievink@gmail.com>
2014-02-23 17:40:01 -0500

    working glitch implementation and image resizing

--- AnonymousBoxWebsite/public/javascripts/controllers.js
+++ AnonymousBoxWebsite/public/javascripts/controllers.js
@@ -1,7 +1,22 @@
 function MessagesCtrl($scope, $http){
+    $scope.glitch = false;
     $http.get('/getmessages').
         success(function(data,status,headers,config){
             console.log("got messages: ", data);
             $scope.messages = data;
+            glitch();
         });
+    var glitch = function(){
+        var sglitch = $scope.glitch
+        $scope.$apply(function(){
+            $scope.glitch = !$scope.glitch;
+            console.log("glitch is ", $scope.glitch);
+        });
+        if(!sglitch){
+            setTimeout(glitch, Math.floor(Math.random()*1000)+200);
+        }else{
+            setTimeout(glitch, Math.floor(Math.random()*10000)+1);
+        }
+                    
+    }
 }
