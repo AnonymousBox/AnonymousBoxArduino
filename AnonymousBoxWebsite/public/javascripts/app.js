1a868e0c22cd04156deeb6ad1dbc94f6505cc47b
Jake Schievink <jakeschievink@gmail.com>
2014-03-19 10:59:37 -0400

    moved Website

--- AnonymousBoxWebsite/public/javascripts/app.js
+++
@@ -1,6 +0,0 @@
-angular.module('Anonymousbox', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
-                $routeProvider.
-                    when('/', { templateUrl: 'partials/messages.jade', controller: MessagesCtrl}).
-                                otherwise({ redirectTo: '/' });
-    }]);
--- AnonymousBoxWebsite/public/javascripts/styling.js
+++
@@ -1,8 +0,0 @@
-
-$(function(){
-    $(document.body).css('padding-top', $('#topnavbar').height() + 40);
-    $('.rimg').resizecrop({
-              width:200,
-              height:200
-    });  
-});
